34e4346c0aca57b2b1b53adb8e2fb34d189ff504
vinnytmv <valopez@mail.csuchico.edu>
2018-09-05 17:37:09 -0700

    Changes greeting to myself introuducing myself

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+/* Author: Vincent Lopez */
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Vincent Lopez. Whats yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
